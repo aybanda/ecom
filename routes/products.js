796f07e2660f84158d1842172f23d769eff136a3
aybanda <160579663+aybanda@users.noreply.github.com>
2024-08-04 10:20:50 +0530

    Create products.js

---
+++ routes/products.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+const { Product } = require("../models/product");
+const { Category } = require("../models/category");
+
+router.get(`/`, async (req, res) => {
+  let filter = {};
+  if (req.query.categories) {
+    filter = { category: req.query.categories.split(",") };
+  }
+
+  const productList = await Product.find(filter).populate("category");
+
+  if (!productList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(productList);
+});
+router.post(`/`, (req, res) => {
+  const product = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    countInStock: req.body.countInStock,
+  });
+  product
+    .save()
+    .then((createdProduct) => {
+      res.status(201).json(createdProduct);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+        success: false,
+      });
+    });
+});
+
+module.exports = router;
